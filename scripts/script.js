c50bdbe6358a092772e19362ba08b7d23dc83b0a
Dane Petersen <dane.m.petersen@gmail.com>
2021-07-23 15:34:50 -0500

    working on trying to get a bootstrap confirm modal to work

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,37 @@
 $(document).ready(onReady);
 
 function onReady() {
-  console.log('hello');
   $(document).on('click', '#addBtn', addEmployee);
 }
 
-function addEmployee() {
-  
+
+function addEmployee(event) {
+  // Stops the forms default behavior
+  // no reload
+  event.preventDefault();
+  $('#exampleModalCenter').modal();
+  checkIfInputIsEmpty();
+}
+
+/**
+  * Check if input is empty
+*/
+function checkIfInputIsEmpty() {
+  let firstName = $('#firstName').val();
+  let lastName = $('#lastName').val();
+  let empId = $('#employeeId').val();
+  let annualSalary = $('#annualSalary').val();
+
+  if ( !firstName || !lastName || !empId || !annualSalary ) {
+    alert('please fill out all of the fields');
+  } else {
+    if ( addEmployee( firstName, lastName, empId, annualSalary) === true ) {
+      // displayItemsInUnOrderedList( year, make, model, price, img );
+      // sumTotalPrice();
+      // clearOutFields();
+      console.log('added');
+    }
+  }
 }
 
 // function getFieldValues() {
